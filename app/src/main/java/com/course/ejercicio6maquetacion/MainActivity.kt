d2336c5d6292123ec76c2fff37c83032cfda0fce
VASS\david.pahissa <david.pahissa@vasscompany.com>
2024-01-30 12:34:25 +0100

    create ViewModel

--- app/src/main/java/com/course/ejercicio6maquetacion/MainActivity.kt
+++ app/src/main/java/com/course/ejercicio6maquetacion/MainActivity.kt
@@ -2,10 +2,22 @@ package com.course.ejercicio6maquetacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.course.ejercicio6maquetacion.Paragraph.ParagraphViewModel
+import com.course.ejercicio6maquetacion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var paragraphViewModel: ParagraphViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
